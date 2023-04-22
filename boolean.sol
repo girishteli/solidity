df44b2020d26f3cd6ff1a1b6231b8ba2f553ff7f
girishteli <girish.n.teli123@gmail.com>
2023-04-22 18:49:12 +0530

    first commit

---
+++ boolean.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.18;
+contract integer{
+    bool public bull;
+    uint public firstnum;
+    uint public secondnum;
+    function setint(uint  x)public{
+        firstnum=x;
+        
+    }
+
+    function setu(uint  y)public{
+        secondnum=y;
+            }
+    function add() view public returns(uint){
+       uint sum = firstnum + secondnum;
+       return sum;
+    }
+}
